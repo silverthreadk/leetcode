3035a03cbb46ac41792f70483ed088919b535b17
siverthreadk <eunsill91@gmail.com>
2019-07-23 21:35:11 +0900

    867_Transpose Matrix

---
+++ Submissions/867_Transpose_Matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        int n = A.size(), m = A[0].size();
+        vector<vector<int> > result (m, vector<int>(n));
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<m; ++j) {
+                result[j][i] = A[i][j];
+            }
+        }
+        
+        return result;
+    }
+};
