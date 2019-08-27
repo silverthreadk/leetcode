43661af7969964b1c4732d57aff60515651318d3
siverthreadk <eunsill91@gmail.com>
2019-08-27 22:27:19 +0900

    637_Average of Levels in Binary Tree

---
+++ Submissions/637_Average_of_Levels_in_Binary_Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            long sum = 0;
+            for(int i=0; i<n; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            result.push_back(sum / static_cast<double>(n));
+        }
+        return result;
+    }
+};
