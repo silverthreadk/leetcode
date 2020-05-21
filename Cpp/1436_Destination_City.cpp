692ea182860a8315224ec0a6f27859e5bdf0d82b
silverthreadk <eunsill91@gmail.com>
2020-05-21 22:04:33 +0900

    1436. Destination City

---
+++ Cpp/1436_Destination_City.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        set<string> s;
+        for(auto& path : paths){
+            s.insert(path[1]);
+        }
+        
+        for(auto& path : paths){
+            s.erase(path[0]);
+        }
+        
+        return *s.begin();
+    }
+};
