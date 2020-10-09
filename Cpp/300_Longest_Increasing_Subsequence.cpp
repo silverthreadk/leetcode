60b3345fd8e6d9790261e62e61615b4ac76d7fd4
silverthreadk <eunsill91@gmail.com>
2020-10-09 17:57:31 +0900

    300. Longest Increasing Subsequence

---
+++ Cpp/300_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        set<int> s;
+        
+        for(int i=0; i<nums.size(); ++i){
+            auto it = s.lower_bound(nums[i]);
+            if(it == s.end()){
+                s.insert(nums[i]);
+            } else {
+                s.erase(it);
+                s.insert(nums[i]);
+            }
+        }
+        
+        return s.size();
+    }
+};
