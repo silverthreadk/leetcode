edb841958bfca3b2152975bc8f8e9310018a510c
silverthreadk <eunsill91@gmail.com>
2021-08-20 23:54:01 +0900

    196. Delete Duplicate Emails

---
+++ sql/196_Delete_Duplicate_Emails.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE Id NOT IN (SELECT *
+                 FROM(SELECT MIN(Id)
+                      FROM Person
+                      GROUP BY Email) as p);
