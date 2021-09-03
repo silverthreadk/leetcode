0732e3b15badfab1937f3b3ae8d219bbb479d1ed
silverthreadk <eunsill91@gmail.com>
2021-09-03 23:58:02 +0900

    1720. Decode XORed Array

---
+++ Cpp/1720_Decode_XORed_Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> result;
+        result.push_back(first);
+        
+        for(int i = 0; i < encoded.size(); ++i) {
+            result.push_back(encoded[i] ^ result[i]);
+        }
+        
+        return result;
+    }
+};
