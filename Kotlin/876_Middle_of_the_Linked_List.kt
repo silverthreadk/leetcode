8dd5a02092cbe4836751b4201b54409d7705c806
silverthreadk <eunsill91@gmail.com>
2022-01-04 18:24:06 +0900

    876. Middle of the Linked List

---
+++ Kotlin/876_Middle_of_the_Linked_List.kt
@@ -0,0 +1,22 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        var fast = head
+        var slow = head
+
+        while(fast?.next != null){
+            fast = fast.next.next
+            slow = slow?.next
+        }
+
+        return slow
+    }
+}
