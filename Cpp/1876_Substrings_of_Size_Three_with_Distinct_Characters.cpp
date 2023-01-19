4071a0dba76d052832fa95a61f0efe051f438ca7
silverthreadk <eunsill91@gmail.com>
2023-01-19 23:27:16 +0900

    1876. Substrings of Size Three with Distinct Characters

---
+++ Cpp/1876_Substrings_of_Size_Three_with_Distinct_Characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int result = 0;
+        int n = s.size();
+        
+        for(int i = 0; i < n - 2; ++i) {
+            if(s[i] != s[i + 1] && s[i] != s[i + 2]&& s[i + 1] != s[i + 2]) ++result;
+        }
+        
+        return result;
+    }
+};
