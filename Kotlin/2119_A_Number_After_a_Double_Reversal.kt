2c7acc0f0280cb85d63190501ad6a75f5b4bba31
silverthreadk <eunsill91@gmail.com>
2023-11-20 21:23:51 +0900

    2119. A Number After a Double Reversal

---
+++ Kotlin/2119_A_Number_After_a_Double_Reversal.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun isSameAfterReversals(num: Int): Boolean {
+        if(num == 0) return true
+        if(num % 10 == 0) return false
+        
+        return true
+    }
+}
